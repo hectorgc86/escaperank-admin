383a26cb04e3276b3efdef4c42edf48e0946657e
Ángel Valera <info@angelvalera.es>
2023-03-29 22:38:36 +0200

    Formulario Noticias

--- src/app/noticias/noticias-form/noticias-form.component.ts
+++ src/app/noticias/noticias-form/noticias-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { DropzoneConfigInterface, DropzoneDirective } from 'ngx-dropzone-wrapper';
 
 @Component({
   selector: 'app-noticias-form',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticias-form.component.scss']
 })
 export class NoticiasFormComponent implements OnInit {
+  selectedDate: NgbDateStruct;
+  public config: DropzoneConfigInterface = {
+    clickable: true,
+    maxFiles: 1,
+    autoReset: null,
+    errorReset: null,
+    cancelReset: null
+  };
+
+  @ViewChild(DropzoneDirective, { static: false }) directiveRef?: DropzoneDirective;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onUploadError(event: any): void {
+    console.log('onUploadError:', event);
+  }
 
+  onUploadSuccess(event: any): void {
+    console.log('onUploadSuccess:', event);
+  }
+
+  resetDropzoneUploads(): void {
+    if (this.directiveRef) {
+      this.directiveRef.reset();
+    }
+  }
 }
--- src/app/noticias/noticias.module.ts
+++ src/app/noticias/noticias.module.ts
@@ -4,7 +4,38 @@ import { NoticiasFormComponent } from './noticias-form/noticias-form.component';
 import { NoticiasListComponent } from './noticias-list/noticias-list.component';
 import { NoticiasCardComponent } from './noticias-card/noticias-card.component';
 import { NoticiasDetailsComponent } from './noticias-details/noticias-details.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ngx-custom-validators';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { DropzoneModule } from 'ngx-dropzone-wrapper';
+import { DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
+import { DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
+const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
+  // Change this to your upload POST address:
+  url: 'https://httpbin.org/post',
+  maxFilesize: 50,
+  acceptedFiles: 'image/*'
+};
+const routes: Routes = [
+  {
+    path: '',
+    component: NoticiasListComponent,
 
+  },
+      {
+        path: 'nueva',
+        component: NoticiasFormComponent
+      },
+      {
+        path: ':id',
+        component: NoticiasDetailsComponent
+      },
+      {
+        path: ':id/edit',
+        component: NoticiasFormComponent
+      }
+]
 
 
 @NgModule({
@@ -14,8 +45,16 @@ import { NoticiasDetailsComponent } from './noticias-details/noticias-details.co
     NoticiasCardComponent,
     NoticiasDetailsComponent
   ],
-  imports: [
-    CommonModule
+  imports: [RouterModule.forChild(routes), CommonModule,FormsModule,
+    ReactiveFormsModule,
+    CustomFormsModule,    NgbDatepickerModule,DropzoneModule
+  ],
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: DROPZONE_CONFIG,
+      useValue: DEFAULT_DROPZONE_CONFIG
+    }, // Ngx-dropzone-wrapper
   ]
 })
 export class NoticiasModule { }
