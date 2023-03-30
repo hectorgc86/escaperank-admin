137c88282d028299913f8a8b76cba73c1e45ae2c
Ángel Valera <info@angelvalera.es>
2023-03-30 19:27:48 +0200

    Formulario Salas

--- src/app/salas/salas-form/salas-form.component.ts
+++ src/app/salas/salas-form/salas-form.component.ts
@@ -1,15 +1,178 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { DropzoneComponent, DropzoneConfigInterface, DropzoneDirective } from 'ngx-dropzone-wrapper';
 
 @Component({
   selector: 'app-salas-form',
   templateUrl: './salas-form.component.html',
   styleUrls: ['./salas-form.component.scss']
 })
-export class SalasFormComponent implements OnInit {
 
+export class SalasFormComponent implements OnInit {
+  dificultades: Dificultad[] = [];
+  selectedDificultadId: string = '';
+  publicos: Publico[] = [];
+  selectedPublico: any = null;
+  tematicas: Tematica[] = [];
+  selectedTematica: any = null;
+
+  categorias: Categoria[] = [];
+  selectedCategoria: any = null;
+
+
+  public config: DropzoneConfigInterface = {
+    clickable: true,
+    maxFiles: 1,
+    autoReset: null,
+    errorReset: null,
+    cancelReset: null
+  };
+  public config2: DropzoneConfigInterface = {
+    clickable: true,
+    maxFiles: 1,
+    autoReset: null,
+    errorReset: null,
+    cancelReset: null
+  };
   constructor() { }
 
+
+  @ViewChild('dz') drpzone?: DropzoneComponent;
+  @ViewChild('dz2') drpzone2?: DropzoneComponent;
+
   ngOnInit(): void {
+    this.dificultades = Dificultades.dificultades;
+    this.publicos = Publicos.publicos;
+    this.tematicas = Tematicas.tematicas;
+    this.categorias = Categorias.categorias;
+  }
+  
+  
+  resetDropzone1Uploads(): void {
+    this.drpzone?.directiveRef?.reset()
+    this.drpzone?.directiveRef?.dropzone();
+   
+  }
+  resetDropzone2Uploads(): void {
+    this.drpzone2?.directiveRef?.reset()
+    this.drpzone2?.directiveRef?.dropzone();
+
+
   }
 
+  onSuccess(event: any){
+    console.log('onSuccessFirstCarPhoto:', event);
+
+  }
+
+  onUploadError(event: any): void {
+    console.log('onUploadError:', event);
+  }
+
+  onUploadSuccess(event: any): void {
+    console.log('onUploadSuccess:', event);
+  }
+  onRemoved(event: any){
+    console.log('onRemovedFirstCarPhoto:', event);
+  }
+  onSending(event: any){
+    console.log('onSending:', event);
+  }
+  onFileDropped(event:any){
+    console.log('onFileDropped:', event);
+
+  }
+  onRemoveFile(event:any){
+    console.log('onRemoveFile:', event);
+  }
 }
+
+export class Dificultades {
+  public static dificultades: Dificultad[] = [
+		// ng-select => peoples
+    {
+      'id': '1',
+     'dificultad':'Baja'
+    },
+    {
+      'id': '2',
+     'dificultad':'media'
+    },
+    {
+      'id': '3',
+     'dificultad':'Alta'
+    }];
+  }
+
+    export interface Dificultad {
+      id: string;
+      dificultad?: string;
+    }
+
+
+    export class Publicos {
+      public static publicos: Publico[] = [
+        // ng-select => peoples
+        {
+          'id': '1',
+         'publico':'Infantil'
+        },
+        {
+          'id': '2',
+         'publico':'Adolescente'
+        },
+        {
+          'id': '3',
+         'publico':'Adultos'
+        }];
+      }
+    
+        export interface Publico {
+          id: string;
+          publico?: string;
+        }
+
+
+        export class Tematicas {
+          public static tematicas: Tematica[] = [
+            // ng-select => peoples
+            {
+              'id': '1',
+             'tematica':'Terror'
+            },
+            {
+              'id': '2',
+             'tematica':'Misterio'
+            },
+            {
+              'id': '3',
+             'tematica':'Comedia'
+            }];
+          }
+        
+            export interface Tematica {
+              id: string;
+              tematica?: string;
+            }
+
+
+            export class Categorias {
+              public static categorias: Categoria[] = [
+                // ng-select => peoples
+                {
+                  'id': '1',
+                 'categoria':'Escape Room'
+                },
+                {
+                  'id': '2',
+                 'categoria':'Hall Escape'
+                },
+                {
+                  'id': '3',
+                 'categoria':'Box'
+                }];
+              }
+            
+                export interface Categoria {
+                  id: string;
+                  categoria?: string;
+                }
--- src/app/salas/salas.module.ts
+++ src/app/salas/salas.module.ts
@@ -4,7 +4,39 @@ import { SalasFormComponent } from './salas-form/salas-form.component';
 import { SalasCardComponent } from './salas-card/salas-card.component';
 import { SalasDetailsComponent } from './salas-details/salas-details.component';
 import { SalasListComponent } from './salas-list/salas-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ngx-custom-validators';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { DropzoneModule } from 'ngx-dropzone-wrapper';
+import { DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
+import { DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
+import { NgSelectModule } from '@ng-select/ng-select';
+const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
+  // Change this to your upload POST address:
+  url: 'https://httpbin.org/post',
+  maxFilesize: 50,
+  acceptedFiles: 'image/*'
+};
+const routes: Routes = [
+  {
+    path: '',
+    component: SalasListComponent,
 
+  },
+      {
+        path: 'nueva',
+        component: SalasFormComponent
+      },
+      {
+        path: ':id',
+        component: SalasDetailsComponent
+      },
+      {
+        path: ':id/edit',
+        component: SalasFormComponent
+      }
+]
 
 
 @NgModule({
@@ -14,8 +46,17 @@ import { SalasListComponent } from './salas-list/salas-list.component';
     SalasDetailsComponent,
     SalasListComponent
   ],
-  imports: [
-    CommonModule
+  imports: [RouterModule.forChild(routes), CommonModule,FormsModule,
+    ReactiveFormsModule,
+    CustomFormsModule,    NgbDatepickerModule,DropzoneModule,    NgSelectModule, // Ng-select
+
+  ],
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: DROPZONE_CONFIG,
+      useValue: DEFAULT_DROPZONE_CONFIG
+    }, // Ngx-dropzone-wrapper
   ]
 })
 export class SalasModule { }
