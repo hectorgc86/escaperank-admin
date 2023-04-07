aae4d133444ce8cd480caf1f1425bbfacc31c4e8
Ángel Valera <info@angelvalera.es>
2023-04-07 16:51:51 +0200

    Cambio URL API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://rocky-mesa-97932.herokuapp.com/escaperank/v2",
+  baseUrl: "https://escaperankapi.herokuapp.com/escaperank/v2",
   storageUrl: "http://132.145.65.153/escaperankstorage/img",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://rocky-mesa-97932.herokuapp.com/escaperank/v2",
+  baseUrl: "https://escaperankapi.herokuapp.com/escaperank/v2",
   storageUrl: "http://132.145.65.153/escaperankstorage/img",
 };
 
