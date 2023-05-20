7eeb448fd5fcc7bad3ba02e1ac92ba242995f13c
Ángel Valera <info@angelvalera.es>
2023-05-20 13:22:22 +0200

    Fondos y menú actualizados

--- src/app/views/layout/sidebar/menuUser.ts
+++ src/app/views/layout/sidebar/menuUser.ts
@@ -30,27 +30,14 @@ export const MENUUSER: MenuItem[] = [
     ],
   },
   {
-    label: "Partidas",
+    label: "Nueva Partida",
     icon: "flag",
-    subItems: [
-      {
-        label: "Listado",
-        link: "/partidas/",
-      },
-      {
-        label: "Nueva",
-        link: "/partidas/nueva",
-      },
-    ],
+    link: "/partidas/nueva",
   },
   {
-    label: "Usuarios",
+    label: "Perfil",
     icon: "user",
-    subItems: [
-      {
-        label: "Perfil",
-        link: "/usuarios/:id",
-      },
-    ],
+    link: "/usuarios/:id",
+   
   },
 ];
